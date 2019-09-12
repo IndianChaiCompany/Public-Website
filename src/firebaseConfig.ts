632759fec48d0dae510304c33939fc9909052f74
Ankur Nigam <ankur.nigam198@gmail.com>
2019-09-12 17:22:25 +0530

    unhiding config

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAT_dNU866al3UFX0mGYff3fi4zGsL0HwY",
+  authDomain: "indianchaicompany.firebaseapp.com",
+  databaseURL: "https://indianchaicompany.firebaseio.com",
+  projectId: "indianchaicompany",
+  storageBucket: "",
+  messagingSenderId: "912570061796",
+  appId: "1:912570061796:web:0e4e4f47cf9ffdcfbd2df1"
+};
